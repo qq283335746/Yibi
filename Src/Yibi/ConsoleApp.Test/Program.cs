4355633b5ffa62e08641fa9b1a664cdc6ae8d103
qq283335746 <283335746@qq.com>
2019-05-03 11:48:48 +0800

    Converter.csproj

---
+++ Src/Yibi/ConsoleApp.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Yibi.Converter;
+
+namespace ConsoleApp.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("正在启动，请稍后...");
+
+            OoxmlConvert.RunTest();
+
+            Console.WriteLine("运行结束，请按任意键结束!");
+
+            Console.Read();
+        }
+    }
+}
