7a3a75012462f8429849326695b2b4905f49bbda
qq283335746 <283335746@qq.com>
2019-05-08 16:40:51 +0800

    Directory.Build.props

--- Src/Yibi/Converter/ConvertHelper.cs
+++ Src/Yibi/Converter/ConvertHelper.cs
@@ -24,6 +24,9 @@ namespace Yibi.Converter
                 switch (fileName)
                 {
                     case "Index":
+                        items.Add(new WordToHtmlInfo { FilePath = filePath, PageName = fileName, PageTitle = prefix + "" });
+                        break;
+                    case "Home":
                         items.Add(new WordToHtmlInfo { FilePath = filePath, PageName = fileName, PageTitle = prefix + "首页" });
                         break;
                     case "Consul":
@@ -31,6 +34,12 @@ namespace Yibi.Converter
                         break;
                     case "Eureka":
                         items.Add(new WordToHtmlInfo { FilePath = filePath, PageName = fileName, PageTitle = prefix + "分布式架构之Eureka" });
+                        break;
+                    case "Apollo":
+                        items.Add(new WordToHtmlInfo { FilePath = filePath, PageName = fileName, PageTitle = prefix + "分布式架构之Apollo（阿波罗）配置中心" });
+                        break;
+                    case "NuGet":
+                        items.Add(new WordToHtmlInfo { FilePath = filePath, PageName = fileName, PageTitle = prefix + ".Net开发之NuGet" });
                         break;
                     default:
                         break;
